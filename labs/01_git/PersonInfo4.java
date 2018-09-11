06ef4583ed377c564e5a97d061debdf74a667ec3
karljr11 <kdupu059@uottawa.ca>
2018-09-11 18:31:52 -0400

    ajouter salutation

--- labs/01_git/PersonInfo4.java
+++ labs/01_git/PersonInfo4.java
@@ -3,12 +3,14 @@ public class PersonInfo4 {
     public static String firstName = "Karl J.";
     public static String lastName = "Dupuy";
     public static int studentNo = 300018154;
+    public static String desc = "Hi c'est Karl";
     public static String description = "Messi est le GOAT";
     
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
         out += " " + firstName + " " + lastName + '.';
         out += SIConstants.STUDENT_NUMBER_FR + studentNo + '.';
+	out += SIConstants.DESCRIPTION_FR + desc + '.';
         out += SIConstants.DESCRIPTION_FR + description + '.';
         out += SIConstants.EOL;
         
@@ -20,6 +22,7 @@ public class PersonInfo4 {
         String out = SIConstants.GREETING_EN;
         out += " " + firstName + " " + lastName + '.';
         out += SIConstants.STUDENT_NUMBER_EN + studentNo + '.';
+	out += SIConstants.DESCRIPTION_EN + desc + '.';
         out += SIConstants.DESCRIPTION_EN + description + '.';
         out += SIConstants.EOL;
         
